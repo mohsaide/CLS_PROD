d7540905dab309c80a017f64781939d0daaa0ed3
Mohammad saide <68466215+mohsaide@users.noreply.github.com>
2023-04-18 04:45:28 +0300

    Validation in mailing js file/code

--- assets/js/mailing.js
+++ assets/js/mailing.js
@@ -2,20 +2,43 @@
 
 function submitForm(event) {
     event.preventDefault(); 
-
     var myForm = document.getElementById("mailing_form");
     var formData = new FormData(myForm); 
 
+
     
-      fetch("http://clsonline.org/assets/php/mailing.php", {
+      fetch("http://localhost/assets/php/mailing.php", {
         method: "POST",
         body: formData
       })
       .then(response => {
+        
+        var mailing_notification = document.getElementById("mailing_notification");
         console.log(response);
+        if (response['status']==200)
+        {
+            mailing_notification.innerHTML = 'Email has been sent!';
+            mailing_notification.style.backgroundColor = 'green';
+        }
+        else if (response['status']==401)
+        {
+            mailing_notification.innerHTML = 'You are not authorized!';
+            mailing_notification.style.backgroundColor = 'Orange';
+        }
+        else
+        {
+            mailing_notification.innerHTML = 'Something went wrong!';
+            mailing_notification.style.backgroundColor = 'red';
+
+        }
+       
       })
       .catch(error => {
-        console.error('js error');
+
+        mailing_notification.innerHTML = 'Something went wrong!';
+        mailing_notification.style.backgroundColor = 'red';
+        console.error('mailing js error');
+        
       });
 
 
